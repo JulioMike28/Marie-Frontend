c77470adfc9ce8f77760361b7c662aeb7acc4103
JulioMike28 <juliomike17@gmail.com>
2020-08-15 14:23:15 -0300

    alterado tudo na manha de 15/08

--- src/screens/home/ButtonSlider.jsx
+++ src/screens/home/ButtonSlider.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 export default (props) =>(
     <React.Fragment>
-        <button className="btnSlider">
-            <h4 style={{fontFamily: "helvetica-w01-light", fontWeight:"200px",color:"white", letterSpacing:"0.3rem", opacity:'1'}}> T-SHIRT</h4>
+        <button style={{background: `${props.background}`, border: `1px solid ${props.sombra}`,boxShadow:`2px 6px ${props.sombra}`}} className="btnSlider">
+            <h4 style={{fontFamily: "helvetica-w01-light", fontWeight:"200px",color:"white", letterSpacing:"0.3rem", opacity:'1'}}> {props.category}</h4>
             <br></br>
-            <div className="apelido">
-                <div className="texto"> 
-                    <p style={{fontFamily:"Cookie", fontSize:"60px",fontWeight:"200"}}>Crush</p>
+            <div style={{background:`${props.sombra}`}} className="apelido">
+                <div style={{position: "absolute",right: `${props.right}`, top: `${props.top}`}}> 
+                    <p style={{fontFamily:"Cookie", fontSize:`${props.font}`}}>{props.apelido}</p>
                 </div>     
             </div>
             <button className="comprar btn">COMPRAR</button>
--- src/screens/home/SlideItemScreen.jsx
+++ src/screens/home/SlideItemScreen.jsx
@@ -8,15 +8,40 @@ export default (props) => (
             <div className="carousel-inner">
                 <div className="carousel-item active">
                     <img className="d-block w-100" src={props.image1} alt="First slide" />
-                    <ButtonSlider/>
+                    <ButtonSlider
+                        background="#afeeee"
+                        sombra="#c8a2c8"
+                        apelido="Miga sua Louca"
+                        category="T-SHIRT"
+                        right="50px"
+                        top="100px"
+                        font="40px"
+                        fundo="#deb887"
+                    />
                 </div>
                 <div className="carousel-item">
                     <img className="d-block w-100" src={props.image2} alt="Second slide" />
-                    <ButtonSlider/>
+                    <ButtonSlider
+                        background="#FFA07A"
+                        sombra="#E0FFFF"
+                        apelido="Crush"
+                        category="MACAQUINHO"
+                        right="90px"
+                        top="90px"
+                        font="60px"
+                    />
                 </div>
                 <div className="carousel-item">
                     <img className="d-block w-100" src={props.image3} alt="Third slide"/>
-                    <ButtonSlider/>
+                    <ButtonSlider
+                        background="#98FB98"
+                        sombra="#DAA520"
+                        apelido="Arrasôu"
+                        category="SHORT"
+                        right="80px"
+                        top="80px"
+                        font="60px"
+                    />
                 </div>
             </div>
             <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
